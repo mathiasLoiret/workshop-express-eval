0ca9d3dc158763bfd98fb4f6a64f50843a883188
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-01-26 16:18:41 +0100

    Exercise 5

--- index.js
+++ index.js
@@ -8,19 +8,46 @@ const port = process.argv[2] || 3000
 
 app.get('/bingo', function(req, res) {
 
-   fs.readFile('src/numbers.txt', function(err, data) {
+  num = req.query.mynumbers
+  win = false
+  fs.readFile('src/numbers.txt', function(err, data) {
 
-     numbers = (data.toString())
-     array = numbers.split("\r\n");
-     array.pop();
-     resultat = array.join(", ");
+    numbers = (data.toString())
+    array = numbers.split("\r\n");
+    array.pop();
+    arrayNum = num.split(",");
+    
+    if (array == arrayNum){
+      win = true
+    }
 
-     res.send(resultat)
-    });
+    resultat = array.join(", ");
 
+    if (num == null){
+      res.send('The bingo game is already started and known numbers are ' +resultat)
+    }else{
 
+      if (win){
+        res.send('Bingo')
+      }else {
+        res.send("The bingo game is already started, sorry your numbers doesn't match with known numbers " + resultat + " ; so you can not say Bingo")
+      }
+
+    }
+
+  });
 })
+
 app.listen(port)
+
+
+
+
+
+
+
+
+
 
 // start server here
 console.log(chalk.green('Hello web server'));
