a3f6b038405aa3ac72df254760ffc61b3cba069e
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-01-26 15:37:12 +0100

    Exercise 4

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 let chalk = require('chalk');
 const app = require('./src/app');
+var fs = require('fs');
 
 // port
 
 const port = process.argv[2] || 3000
 
 app.get('/bingo', function(req, res) {
-  res.send('Bingo')
+
+   fs.readFile('src/numbers.txt', function(err, data) {
+
+     numbers = (data.toString())
+     array = numbers.split("\r\n");
+     array.pop();
+     resultat = array.join(", ");
+
+     res.send(resultat)
+    });
+
+
 })
 app.listen(port)
 
