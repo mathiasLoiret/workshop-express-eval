bfa1fafd8445e572ce7557f78f20a01c4c82b023
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-01-26 15:04:40 +0100

    Exercise 1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let chalk = require('chalk');
-
 const app = require('./src/app');
 
 // port
+
 const port = 3000;
+
+app.listen(port)
 
 // start server here
 console.log(chalk.green('Hello web server'));
